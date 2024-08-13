04839c03c608435a116d6d12db765c8943ea3716
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-13 21:27:48 +0200

    refactor: simplified code

--- app/app/sanity/loader.server.ts
+++ app/app/sanity/loader.server.ts
@@ -1,7 +1,7 @@
-import * as queryStore from '@sanity/react-loader'
+import { setServerClient, loadQuery } from '@sanity/react-loader'
 import { client } from '~/sanity/client'
 
-export const { loadQuery } = queryStore
+export { loadQuery }
 
 // We need to set the client used by `loadQuery` here, it only affects the server and ensures the browser bundle isn't bloated
-queryStore.setServerClient(client)
+setServerClient(client)
