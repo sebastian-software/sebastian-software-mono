b568dc430538e314502abd210bcb01b3cba2a732
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-13 21:36:11 +0200

    fix: cleanup + added hint on env variables

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -14,6 +14,9 @@ import { Suspense, lazy } from 'react'
 const LiveVisualEditing = lazy(() => import('~/components/LiveVisualEditing'))
 
 export const loader = () => {
+  // Note: This follows the recommendation of Remix to not inject
+  // env at built time, but instead at runtime from the server.
+  // https://remix.run/docs/en/main/guides/envvars#browser-environment-variables
   return json({
     ENV: {
       SANITY_STUDIO_PROJECT_ID: process.env.SANITY_STUDIO_PROJECT_ID,
@@ -39,33 +42,13 @@ export default function App() {
         <div className="container">
           <header className="header">
             <Link className="header__title" to="/">
-              Remix + Sanity
+              Sebastian Software GmbH
             </Link>
           </header>
           <main>
             <Outlet />
           </main>
-          <footer className="footer">
-            <p className="footer__text">
-              Made with{' '}
-              <svg
-                datasanity-icon="heart-filled"
-                width="1em"
-                height="1em"
-                viewBox="0 0 25 25"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M17 16C15.8 17.3235 12.5 20.5 12.5 20.5C12.5 20.5 9.2 17.3235 8 16C5.2 12.9118 4.5 11.7059 4.5 9.5C4.5 7.29412 6.1 5.5 8.5 5.5C10.5 5.5 11.7 6.82353 12.5 8.14706C13.3 6.82353 14.5 5.5 16.5 5.5C18.9 5.5 20.5 7.29412 20.5 9.5C20.5 11.7059 19.8 12.9118 17 16Z"
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeWidth="1.2"
-                ></path>
-              </svg>{' '}
-              at Sanity
-            </p>
-          </footer>
+          <footer className="footer">Copyright Sebastian Software GmbH</footer>
         </div>
         <ScrollRestoration />
         <script
