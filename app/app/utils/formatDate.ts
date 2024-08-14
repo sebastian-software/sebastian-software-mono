a1487970550cf76aa8e3a1a72128df4f13a5a1c2
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-14 21:58:38 +0200

    feat: added non-value support for formatting dates

--- app/app/utils/formatDate.ts
+++ app/app/utils/formatDate.ts
@@ -1,4 +1,8 @@
-export function formatDate(date: string) {
+export function formatDate(date: string | null) {
+  if (!date) {
+    return ''
+  }
+
   return new Date(date).toLocaleDateString('de-DE', {
     month: 'long',
     day: 'numeric',
