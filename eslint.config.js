6c8cbd73dca8d33223d1bfb98649c8793edbf872
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-27 15:42:31 +0200

    feat: added more tool infrastructure

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import { main } from "@effective/eslint-config"
+
+export default [
+  {
+    ignores: [".*/**/*", "build/**/*", "public/**/*", "sst-env.d.ts"]
+  },
+  main,
+  {
+    files: ["**/*.css.ts"],
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase"],
+          leadingUnderscore: "forbid",
+          trailingUnderscore: "forbid",
+          filter: {
+            regex: "[- ]",
+            match: false
+          }
+        }
+      ]
+    }
+  }
+]
