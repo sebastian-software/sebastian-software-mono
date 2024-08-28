7bf2706497d402b29bce3d2ef7b4b040b212e04e
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-28 11:59:49 +0200

    fix: cleanup top-level schema

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,18 +1,21 @@
-import "./localeStringType"
-
 import { addressType } from "./address"
 import { companyType } from "./company"
 import { consultantType } from "./consultant"
 import { humanType } from "./human"
+import {
+  localeBlockContent,
+  localeString,
+  localeText
+} from "./localeStringType"
 import { pictureType } from "./picture"
 import { projectType } from "./project"
 import { testimonialType } from "./testimonial"
-import { localeString, localeText } from "./localeStringType"
 
 export const schemaTypes = [
   // Base Types
   localeString,
   localeText,
+  localeBlockContent,
 
   // Actual Schemas
   addressType,
