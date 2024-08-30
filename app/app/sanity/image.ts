5308011ae8ac3ecf027d64a09def95a8c0cdbfd0
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-30 13:56:30 +0200

    fix: fixed typing issue

--- app/app/sanity/image.ts
+++ app/app/sanity/image.ts
@@ -1,10 +1,9 @@
 import imageUrlBuilder from "@sanity/image-url"
-import type { Image } from "@sanity/types"
 
 import { dataset, projectId } from "~/sanity/env"
 
 const builder = imageUrlBuilder({ projectId, dataset })
 
-export function urlFor(source: Image) {
+export function urlFor(source: string) {
   return builder.image(source)
 }
