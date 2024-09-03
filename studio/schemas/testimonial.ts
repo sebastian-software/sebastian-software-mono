8bfdab2a205fb0fcbb295eff3089d4114b673d39
Sebastian Werner <s.werner@sebastian-software.de>
2024-09-03 09:37:34 +0200

    refactor: use new type for position field

--- studio/schemas/testimonial.ts
+++ studio/schemas/testimonial.ts
@@ -52,7 +52,7 @@ export const testimonialType = defineType({
 
     defineField({
       name: "position",
-      type: "localeString"
+      type: "internationalizedArrayString"
     }),
 
     defineField({
