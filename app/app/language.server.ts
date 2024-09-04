d5b52a66611f7bb698cd8503c3bf6189b1e01375
Sebastian Werner <s.werner@sebastian-software.de>
2024-09-04 22:24:21 +0200

    fix: fixed more TS issues

--- app/app/language.server.ts
+++ app/app/language.server.ts
@@ -3,9 +3,12 @@ import acceptLanguage from "accept-language-parser"
 
 export const languageCookie = createCookie("language", {})
 
-export async function getAppLanguage(request) {
+export async function getAppLanguage(request: Request) {
   const headers = request.headers
-  const languages = acceptLanguage.parse(headers.get("Accept-Language"))
+  const acceptLangHeader = headers.get("Accept-Language")
+  const languages = acceptLangHeader
+    ? acceptLanguage.parse(acceptLangHeader)
+    : []
 
   const browserLanguage = languages[0]?.code
   const cookieLanguage = await languageCookie.parse(headers.get("Cookie"))
@@ -14,10 +17,14 @@ export async function getAppLanguage(request) {
   return appLanguage
 }
 
+interface Messages {
+  messages: Record<string, string>
+}
+
 // Using glob to import all the .po files in the locales directory
 // which is way easier to maintain than manually importing each locale
 // specific file.
-const messages = import.meta.glob("./locales/*.po", { eager: true })
+const messages = import.meta.glob<Messages>("./locales/*.po", { eager: true })
 
 export function getMessages(locale: string) {
   // Glob imports use the path name as the key to the module
