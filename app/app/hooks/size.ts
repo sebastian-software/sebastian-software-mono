e04ce57b4d2b715e8fd286bd31021e42572de06f
Sebastian Werner <s.werner@sebastian-software.de>
2024-09-24 11:30:32 +0200

    feat: added size hook

---
+++ app/app/hooks/size.ts
@@ -0,0 +1,50 @@
+import type { RefObject } from "react"
+import { useEffect, useRef, useState } from "react"
+
+interface UseSizeResult {
+  ref: RefObject<HTMLDivElement>
+  rect: DOMRectReadOnly
+}
+
+function useSize(): UseSizeResult {
+  const [rect, setRect] = useState<DOMRectReadOnly>({
+    width: 0,
+    height: 0,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    x: 0,
+    y: 0,
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    toJSON: () => ({})
+  })
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const element = ref.current
+    if (!element) {
+      return
+    }
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      if (entries.length === 0) {
+        return
+      }
+
+      setRect(entries[0].contentRect)
+    })
+
+    resizeObserver.observe(element)
+
+    // Clean up observer on component unmount
+    return () => {
+      resizeObserver.disconnect()
+    }
+  }, [])
+
+  return { ref, rect }
+}
+
+export default useSize
