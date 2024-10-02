67a079f6bab73abd39e7cf58a04c26ddc3f2ebd2
Sebastian Werner <s.werner@sebastian-software.de>
2024-10-02 21:27:50 +0200

    refactor: use Sanity for content

--- app/app/routes/consulting.tsx
+++ app/app/routes/consulting.tsx
@@ -1,67 +1,40 @@
+import { PortableText } from "@portabletext/react"
+import type { LoaderFunctionArgs } from "@remix-run/node"
+import { loadQuery } from "@sanity/react-loader"
+import type { PAGES_QUERYResult } from "sanity.types"
+
 import { RichText } from "~/components/richtext/RichText"
+import { SanityPortableImage } from "~/components/sanity-image"
+import { useSanityData } from "~/hooks/data"
+import { getAppLanguage } from "~/language.server"
+import { PAGES_QUERY } from "~/queries/pages"
+import { postProcessData } from "~/utils/blockHandler"
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const language = await getAppLanguage(request)
+  const params = { language, id: "consulting" }
+
+  const initial = await loadQuery<PAGES_QUERYResult>(PAGES_QUERY, params)
+  const modified = await postProcessData(initial)
+
+  return { initial: modified, query: PAGES_QUERY, params }
+}
 
 export default function Index() {
+  const { data } = useSanityData<typeof loader>()
+
   return (
     <RichText>
-      <h1>Consulting-Angebote</h1>
+      <h1>{data[0].title}</h1>
 
-      <h2>Unsere Dienstleistungen</h2>
-      <p>
-        Wir bieten maßgeschneiderte Lösungen, um Ihren spezifischen
-        Anforderungen gerecht zu werden:
-      </p>
-
-      <ol>
-        <li>
-          Komplette Entwicklung zu einem Festpreis – Eine transparente,
-          kosteneffiziente Lösung, die Ihnen Sicherheit und Planbarkeit für Ihr
-          Projekt gewährleistet.
-        </li>
-        <li>
-          Langzeitunterstützung – Vor Ort oder Remote – Flexibilität, die sich
-          Ihrem Arbeitsstil anpasst, um eine nahtlose Integration und
-          fortlaufende Unterstützung zu gewährleisten.
-        </li>
-        <li>
-          Beratung, Architektur und Technologie-Evaluierung – Tiefe Einblicke
-          und fachkundige Bewertungen, um die optimale Technologieauswahl für
-          Ihr Projekt sicherzustellen.
-        </li>
-        <li>
-          Vorträge, Schulungen und Mentoring – Wissensvermittlung durch
-          Experten, die darauf abzielt, Ihr Team zu stärken und auf die nächste
-          Stufe zu heben.
-        </li>
-      </ol>
-
-      <h2>Ihr Partner für die Zukunft</h2>
-
-      <p>
-        Als Ihr Partner verpflichten wir uns, nicht nur den aktuellen
-        technischen Herausforderungen zu begegnen, sondern auch zukunftsweisende
-        Lösungen zu entwickeln. Unser Engagement für Exzellenz, unsere
-        Spezialisierung auf React und unser Fokus auf nachhaltige Entwicklung
-        machen uns zum idealen Begleiter auf Ihrem Weg in eine erfolgreiche
-        digitale Zukunft.
-      </p>
-
-      <h2>Unsere Wertschätzung für Empfehlungen</h2>
-      <p>
-        Seit unseren Anfängen, sei es als Freelancer oder nun innerhalb unseres
-        eigenen Unternehmens, sind wir uns der Bedeutung direkter Kontakte und
-        Empfehlungen bewusst. Die meisten unserer Projekte entstanden durch
-        Empfehlungen von Entwicklern, Produktmanagern, Designern und anderen,
-        die mit unserer Arbeit vertraut sind. Diese Form der Anerkennung und des
-        Vertrauens in unsere Arbeit schätzen wir zutiefst.
-      </p>
-
-      <p>
-        Als Zeichen unserer Dankbarkeit möchten wir jedem Empfehler neuer
-        Projekte, dessen Unterstützung zu einer Vertragsunterzeichnung führt,
-        einen 100€ Amazon-Gutschein als Dankeschön anbieten. Ihre Empfehlungen
-        sind das höchste Lob für uns und ein entscheidender Faktor unseres
-        gemeinsamen Erfolgs.
-      </p>
+      <PortableText
+        value={data[0].content}
+        components={{
+          types: {
+            picture: SanityPortableImage
+          }
+        }}
+      />
     </RichText>
   )
 }
