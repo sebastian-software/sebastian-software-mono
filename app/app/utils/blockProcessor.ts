e6ecb76c1a0d7ce63c0bbedd501754f97f8952e7
Sebastian Werner <s.werner@sebastian-software.de>
2024-10-10 11:41:17 +0200

    fix: fixed typing of page... next initial

--- app/app/utils/blockProcessor.ts
+++ app/app/utils/blockProcessor.ts
@@ -1,8 +1,6 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable @typescript-eslint/require-await */
 
-import type { Merge } from "type-fest"
-
 import { processPictureBlock } from "./pictureHandler"
 
 /**
@@ -18,39 +16,39 @@ export interface SanityBasePage extends Record<string, unknown> {
   content: SanityPortableBlock[] | ProcessedBlock[]
 }
 
-function isPage(page: unknown): page is SanityBasePage {
-  return (
-    typeof page === "object" &&
-    page != null &&
-    "content" in page &&
-    Array.isArray(page.content)
-  )
-}
-
 type ProcessedBlock = Awaited<ReturnType<(typeof plugins)[number]>>
 
-export async function postProcessData<TData extends Record<string, unknown>>(
-  data: TData
-) {
-  const page = data.page
-  if (isPage(page)) {
+//
+
+export async function postProcessData<TData extends object>(data: TData) {
+  if (
+    "page" in data &&
+    typeof data.page === "object" &&
+    data.page != null &&
+    "content" in data.page &&
+    Array.isArray(data.page.content)
+  ) {
+    const page = data.page
     const content = page.content
-    const modifiedContent: ProcessedBlock[] = []
-    for (const block of content) {
-      let modifiedBlock = block
-      for (const plugin of plugins) {
-        modifiedBlock = await plugin(modifiedBlock)
+
+    if (Array.isArray(content)) {
+      const modifiedContent: ProcessedBlock[] = []
+      for (const block of content) {
+        let modifiedBlock = block
+        for (const plugin of plugins) {
+          modifiedBlock = await plugin(modifiedBlock)
+        }
+
+        modifiedContent.push(modifiedBlock)
       }
 
-      modifiedContent.push(modifiedBlock)
+      const modifiedPage = { ...page, content: modifiedContent } as Omit<
+        NonNullable<(typeof data)["page"]>,
+        "content"
+      > & { content: ProcessedBlock[] }
+
+      return { content: modifiedContent, page: modifiedPage }
     }
-
-    const modifiedPage: Merge<
-      typeof page,
-      { content: typeof modifiedContent }
-    > = { ...page, content: modifiedContent }
-
-    return { content: modifiedContent, page: modifiedPage }
   }
 
   return { data }
