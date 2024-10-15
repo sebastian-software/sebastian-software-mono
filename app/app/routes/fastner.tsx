a6f21eb1574f58c2f5173fbccda0e5e67a91b630
Sebastian Werner <s.werner@sebastian-software.de>
2024-10-15 12:11:10 +0200

    feat: added headshot processing

--- app/app/routes/fastner.tsx
+++ app/app/routes/fastner.tsx
@@ -7,6 +7,8 @@ import { getAppLanguage } from "~/language.server"
 import { ProjectList } from "~/pages/profile"
 import { PROJECTS_QUERY } from "~/queries/projects"
 import { loadQuery } from "~/sanity/loader.server"
+import { replaceFieldAtPathString } from "~/utils/dataUtil"
+import { processPicture } from "~/utils/pictureHandler"
 
 export const meta: MetaFunction = () => {
   return [{ title: "Sebastian Software GmbH" }]
@@ -19,19 +21,35 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   }
 
   const initial = await loadQuery<PROJECTS_QUERYResult>(PROJECTS_QUERY, params)
-  return { initial, query: PROJECTS_QUERY, params }
+
+  const modifiedHeadshot = await processPicture(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    initial.data.consultant!.headshot
+  )
+
+  const modifiedInitial = replaceFieldAtPathString(
+    initial,
+    "data.consultant.headshot",
+    modifiedHeadshot
+  )
+
+  return { initial: modifiedInitial, query: PROJECTS_QUERY, params }
 }
 
 export default function ProfileWerner() {
   const { data, params, encodeDataAttribute } = useSanityData<typeof loader>()
+  const { consultant, projects } = data
 
   return (
     <section>
-      <ProjectList
-        name={params.name}
-        data={data}
-        encodeDataAttribute={encodeDataAttribute}
-      />
+      {consultant && (
+        <ProjectList
+          name={params.name}
+          consultant={consultant}
+          projects={projects}
+          encodeDataAttribute={encodeDataAttribute}
+        />
+      )}
     </section>
   )
 }
