0a135073465d1eba6ebe46ba7f4e63157b12282d
Sebastian Werner <s.werner@sebastian-software.de>
2024-11-05 20:25:56 +0100

    fix: fixed lingui config

--- app/lingui.config.ts
+++ app/lingui.config.ts
@@ -1,10 +1,8 @@
 import type { LinguiConfig } from "@lingui/conf"
 
 const config: LinguiConfig = {
-  fallbackLocales: {
-    default: "en"
-  },
   locales: ["en", "de"],
+  sourceLocale: "en",
   catalogs: [
     {
       path: "<rootDir>/app/locales/{locale}",
